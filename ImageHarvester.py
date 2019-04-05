eb3f706ad97aa4d44a32a40bba60dd00963a3275
Alex Z <Ziegenkonig@users.noreply.github.com>
2019-04-05 15:10:01 -0500

    Updating for new reddit layout, improved naming techniques

--- ImageHarvester.py
+++ ImageHarvester.py
@@ -2,25 +2,29 @@ import urllib2;
 from bs4 import BeautifulSoup;
 
 #Declaring URL and folder_path
-masterURL = "https://www.reddit.com/r/ImaginaryLeviathans/";
+masterURL = "https://old.reddit.com/r/ImaginaryLeviathans/";
 URL = "";
-folder_path = "C:/Users/t0rmsanz/Pictures/HarvestedImages/harvest";
+folder_path = "C:/Users/t0rmsanz/Pictures/HarvestedImages/";
 
 
 #Requesting access as a user-agent that is not a web drone, and also pulling the info
 url_request = urllib2.Request( masterURL, headers = {"User-Agent": "Mozilla/5.0"} );
 
+print('Request has been created. . .' + '\n');
+print('Retrieving page contents. . .' + '\n');
+
 page_contents = BeautifulSoup( urllib2.urlopen(url_request) );
 
-images = page_contents.findAll('a', 'thumbnail');
-print( len(images) );
+print('Contents of page have been retrieved!' + '\n');
+print('Now initializing downloads. . .' + '\n');
 
-index = 0;
+images = page_contents.findAll('a', 'thumbnail');
+
 for image in images:
 	
 	#Setting url to the current image url in list
 	URL = image['href'];
-	print(URL); #Looks nice to see which files are being downloaded
+	print('Downloading: \n' + URL); #Looks nice to see which files are being downloaded
 	
 	while True:
 		try:
@@ -28,13 +32,19 @@ for image in images:
 			url_request = urllib2.Request( URL, headers = {"User-Agent": "Mozilla/5.0"} );
 			url_response = urllib2.urlopen(url_request);
 			
-			path = (folder_path + str(index+1) + '.jpg');
+			#Finds '.jpg' in url
+			#Finds first '/' that occurs before the '.jpg'
+			#Finds all text between '/' and '.jpg'
+			#If you're looking at this, I'm sorry; this could be done better in a regex
+			name = URL[URL[:URL.find('.jpg')].rfind( '/' )+1:URL.find('.jpg')];
+			
+			path = (folder_path + name + '.jpg');
+			
 			#Writing the pictures to the files
 			f = open( path, 'wb' );
 			f.write(url_response.read());
 			f.close();
 			
-			index += 1;
 			break;
 		except urllib2.URLError:
 			print('UNABLE TO RETRIEVE IMAGE FROM: ' + URL);
